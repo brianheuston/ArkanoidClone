2abba12d9b76bf1f68887f010f20f2baa5b5d376
Brian Heuston <bah5069@gmail.com>
2014-11-17 21:38:10 -0800

    GitHub #1 - Ship can leave borders of playing field
    - Added ways to dynamically detect the size of the playing field
    - Added checks to not allow the ship to leave these bounds

--- Assets/Resources/Scripts/MoveScript.cs
+++ Assets/Resources/Scripts/MoveScript.cs
@@ -3,10 +3,27 @@ using System.Collections;
 
 public class MoveScript : MonoBehaviour {
     public float speed = 3.0f;
+    private float originalY;
+    private float maxX = 85; // Default value
 
 	// Use this for initialization
 	void Start () {
-	
+        originalY = transform.position.y;
+        GameObject leftBorder = GameObject.Find("border_left");
+        GameObject rightBorder = GameObject.Find("border_right");
+
+        // The game boundaries are:
+        // ((Left border + border width + racket width), (Right border - border width - racket width))
+        if (leftBorder != null && rightBorder != null)
+        {
+            float newMaxX = rightBorder.transform.position.x;
+            if (rightBorder.collider2D != null)
+            {
+                newMaxX -= ((BoxCollider2D)rightBorder.collider2D).size.x / 2.0f;
+            }
+
+            newMaxX -= ((BoxCollider2D)transform.collider2D).size.x / 2.0f;
+        }
 	}
 	
 	// Update is called once per frame
@@ -23,6 +40,20 @@ public class MoveScript : MonoBehaviour {
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(new Vector2(speed, 0));
+        }
+
+        if (transform.position.y != originalY)
+        {
+            transform.Translate(new Vector2(0, originalY - transform.position.y));
+        }
+
+        if (transform.position.x > maxX)
+        {
+            transform.Translate(new Vector2((maxX - transform.position.x), 0));
+        }
+        if (transform.position.x < -maxX)
+        {
+            transform.Translate(new Vector2(-transform.position.x - maxX, 0));
         }
     }
 }
